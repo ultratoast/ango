eacaddb48ae1a102e41b162fee623d98cb7f113e
John Perea <john@metaworks.co>
2014-10-29 18:13:12 -0700

    uploading a bunch of pretty stuff

---
+++ scripts/controllers/player.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngbpApp.controller:PlayerCtrl
+ * @description
+ * # PlayerCtrl
+ * Controller of the ngbpApp
+ */
+angular.module('ngbpApp')
+  .controller('PlayerCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    
+  });
